e83938e09d0258b1df3c23e1fc26057d737ff9e6
rishabh <rishabh23kothari@gmail.com>
2022-03-10 20:03:46 +0530

    Second Commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,7 +14,7 @@ class Contact extends Component {
                         
                       <center>
                       <h1>Contact Us</h1>
-                      <p><h5>How Can We Help ? Reach Us via Email or Contact Number</h5><br/><br/><br/>
+                      <p><h5>How Can We Help ? Reach Us using Email or Contact Number</h5><br/><br/><br/>
                       Email Id:examportal@gmail<br/>
                       Contact No:8888535324
                       </p>
